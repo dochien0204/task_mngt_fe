7cf93d2d50d26e10425a50c2b6592e27bc576a01
docong2708 <dxcong2708@gmail.com>
2024-05-02 00:41:07 +0700

    feat: complete project screen

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,33 @@ import Activity from "./Activity";
 import CalendarBoard from "./CalendarBoard";
 
 const Projects = () => {
+  const onChange = (key) => {
+    console.log(key);
+  };
+
+  const itemsTab = [
+    {
+      label: "Tasks",
+      key: "1",
+      children: <Tasks />,
+    },
+    {
+      label: "Kanban",
+      key: "2",
+      children: <KanbanBoard />,
+    },
+    {
+      label: "Calendar",
+      key: "3",
+      children: <CalendarBoard />,
+    },
+    {
+      label: "Activity",
+      key: "4",
+      children: <Activity />,
+    },
+  ];
+
   return (
     <Tabs
       onChange={onChange}
