bc705a16d9c980f5b07d1610e7e69d271dba98a7
docong2708 <dxcong2708@gmail.com>
2024-05-02 00:45:50 +0700

    fix: modify app js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 import Wrapper from "./components/Wrapper";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
-import KanbanBoard from "./components/KanbanBoard";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Projects from "./components/Projects";
+import Members from "./components/Members";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Wrapper />}>
         <Route path="" element={<Dashboard />} />
-        <Route path="tasks" element={<KanbanBoard />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="members" element={<Members />} />
       </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
