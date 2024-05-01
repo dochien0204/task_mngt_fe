2af04b2e230f2812e3fb32d0a4a340b7799e5958
docong2708 <dxcong2708@gmail.com>
2024-05-02 00:36:46 +0700

    fix: fix nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,15 +6,15 @@ export default function Nav() {
 
   const items = [
     {
-      id: "1",
-      path: "/",
-      navName: "Dashboard",
-      icon: "icon-grid",
+      id: "2",
+      path: "/projects",
+      navName: "Projects",
+      icon: "icon-paper",
     },
     {
-      id: "2",
-      path: "/tasks",
-      navName: "Tasks",
+      id: "3",
+      path: "/members",
+      navName: "Members",
       icon: "icon-paper",
     },
   ];
@@ -26,29 +26,68 @@ export default function Nav() {
   React.useEffect(() => {
     const pathname = window.location.pathname;
     switch (pathname) {
-      case "/tasks":
-        setActiveLink("2")
+      case "/projects":
+        setActiveLink("2");
         break;
+      case "/members":
+        setActiveLink("3");
       default:
         break;
     }
-  }, [])
+  }, []);
 
   return (
     <nav className="sidebar sidebar-offcanvas" id="sidebar">
       <ul className="nav">
+        <li
+          className={`${activeLink === 0 ? "active" : ""} nav-item`}
+          key={Math.random()}
+          onClick={() => activeNav(0)}
+          style={{ border: "1px solid #ddd", borderRadius: 8 }}
+        >
+          <Link className="nav-link" to={"/"}>
+            <i
+              className={`icon-grid menu-icon`}
+              style={{ marginTop: "-6px" }}
+            ></i>
+            <span className="menu-title">Dashboard</span>
+          </Link>
+        </li>
+        <li style={{ paddingLeft: 16, paddingTop: 10, fontWeight: "bold" }}>
+          <span className="menu-title">PROJECTS</span>
+        </li>
         {items.map((item, _) => (
           <li
             className={`${activeLink === item.id ? "active" : ""} nav-item`}
             key={Math.random()}
             onClick={() => activeNav(item.id)}
+            style={{ border: "1px solid #ddd", borderRadius: 8 }}
           >
             <Link className="nav-link" to={item.path}>
-              <i className={`${item.icon} menu-icon`}></i>
+              <i
+                className={`${item.icon} menu-icon`}
+                style={{ marginTop: "-6px" }}
+              ></i>
               <span className="menu-title">{item.navName}</span>
             </Link>
           </li>
         ))}
+        <li
+          style={{
+            paddingLeft: 16,
+            paddingTop: 10,
+            fontSize: 14,
+            cursor: "pointer",
+          }}
+        >
+          <div style={{ display: "flex", gap: 6, alignItems: "center" }}>
+            <i
+              className="icon-plus menu-icon"
+              style={{ marginTop: "-6px" }}
+            ></i>
+            <span>Add a Project</span>
+          </div>
+        </li>
       </ul>
     </nav>
   );
