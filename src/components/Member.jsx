a44b1a525157adbe8a3ca281b094e6496dfabeb1
docong2708 <dxcong2708@gmail.com>
2024-05-02 00:33:57 +0700

    feture: initial member screen

---
+++ src/components/Member.jsx
@@ -0,0 +1,48 @@
+import {UpCircleOutlined, ClockCircleOutlined} from "@ant-design/icons";
+import { Button, Divider, Tag, Timeline } from "antd";
+
+const Members = () => {
+    const items = [
+        {
+            id: Math.random(),
+            name: "Members",
+            elements: [
+                {
+                    key: Math.random(),
+                    name: "Davea Butler",
+                    position: "Team Lead",
+                    task: "10",
+                },
+                {
+                    key: Math.random(),
+                    name: "Ronaldo",
+                    position: "Team Lead",
+                    task: "10",
+                },
+                {
+                    key: Math.random(),
+                    name: "Messi",
+                    position: "Team Lead",
+                    task: "10",
+                },
+                {
+                    key: Math.random(),
+                    name: "Bui Ngoc Thang",
+                    position: "Team Lead",
+                    task: "10",
+                },
+                {
+                    key: Math.random(),
+                    name: "Hoang Quoc Viet",
+                    position: "Team Lead",
+                    task: "10",
+                },
+                {
+                    key: Math.random(),
+                    name: "Nguyen Cong Hoan",
+                    position: "Team Lead",
+                    task: "3",
+                },
+            ],
+        },
+    ];
