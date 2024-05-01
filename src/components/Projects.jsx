47c1a9963c2ba916a9382e436fb5cf5db9ce0c32
docong2708 <dxcong2708@gmail.com>
2024-05-02 00:40:40 +0700

    feat: add project jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,3 +3,22 @@ import KanbanBoard from "./KanbanBoard";
 import Tasks from "./Tasks";
 import Activity from "./Activity";
 import CalendarBoard from "./CalendarBoard";
+
+const Projects = () => {
+  return (
+    <Tabs
+      onChange={onChange}
+      type="card"
+      items={itemsTab.map((item, i) => {
+        const id = String(i + 1);
+        return {
+          label: item.label,
+          key: id,
+          children: item.children,
+        };
+      })}
+    />
+  );
+};
+
+export default Projects;
