6cd4481dcc962a86ac327628cef885775123374b
docong2708 <dxcong2708@gmail.com>
2024-05-02 00:39:03 +0700

    feat: add project screen

---
+++ src/components/Projects.jsx
@@ -0,0 +1,5 @@
+import { Tabs } from "antd";
+import KanbanBoard from "./KanbanBoard";
+import Tasks from "./Tasks";
+import Activity from "./Activity";
+import CalendarBoard from "./CalendarBoard";
